e927e76396a46e89bb5e95b433af0298855c810e
GHADY-MATTA <ghady5255@gmail.com>
2025-05-14 17:15:59 +0300

    Read input data from stdin

--- server/fetch_transcript.py
+++ server/fetch_transcript.py
@@ -3,4 +3,5 @@ import json
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import parse_qs, urlparse
- Initialize Python script
+sys.stdout.reconfigure(encoding='utf-8')
+data = sys.stdin.read()
